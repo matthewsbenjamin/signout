4214471ceffb66d3d5af51bbb03e25352585d1d6
Ben Matthews <89benmatthews@gmail.com>
2019-12-31 16:29:10 +0000

    Changing boat validation script

--- scripts/boat-validation.js
+++ scripts/boat-validation.js
@@ -1,22 +1,3 @@
-
-const boatInput = document.getElementById('newBoatInput');
-
-boatInput.addEventListener('input', update);
-
-async function newBoatValidation() {
-
-    try {
-        const url = `http://localhost:8080/api/?boat`;
-        const result = await fetch(url);
-        const data = await result.json();
-        return data;
-
-    } catch(error) {
-        console.log(error);
-
-    }
-};
-
 async function newBoatTextUpdate() {
 
     const boatName = await newBoatValidation(boatInput.value);
@@ -25,6 +6,35 @@ async function newBoatTextUpdate() {
 };
 
 
-function update() {
-    newBoatTextUpdate();
+
+
+async function getBoatList() {
+    try {
+        const url = `http://localhost:8080/api?boat`;
+        const result = await fetch(url);
+        const data = await result.json();
+        return data;
+
+    } catch(error) {
+        console.log(error);
+
+    }
 }
+
+// on load - get the data from the api
+document.onload = function() {
+
+    const boats = await getBoatList();
+
+    const element = document.getElementById('newBoatInput');
+    element.addEventListener('input', update);
+    
+
+
+};
+
+// add a checker to validate the string 
+
+
+// when valid - allow user to submit
+
