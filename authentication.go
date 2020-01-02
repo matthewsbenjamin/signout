bb3db6c66a2028d0e6a7529124ca0d86ab549f1b
Ben Matthews <89benmatthews@gmail.com>
2020-01-02 21:37:45 +0000

    changing how authentication occurs - should now work

--- authentication.go
+++ authentication.go
@@ -44,35 +44,25 @@ func isLoggedIn(req *http.Request) bool {
 	return true
 }
 
-// authenticate will return true if the user is logged in
-// todo - enrich the return value OR create userData func to return logged in data
-// why is this needed - different from isLoggedIn?
-func authenticate(req *http.Request) bool {
+// is user will return bool when it matches a user in the database
+func isUser(u string) bool {
 
-	uid, err := req.Cookie("uid")
-	if err != http.ErrNoCookie {
-
-		// they already have a cookie
-		db, err := sql.Open("mysql", dbCreds)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
-
-		// get the corresponding uid from the database
-		// then scan the count of uid into ct
-		var ct int
-		err = db.QueryRow("SELECT count(uid) FROM logins WHERE uid =  ?", uid.Value).Scan(&ct)
-		if err == sql.ErrNoRows {
-			return false
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		return ct > 0
+	db, err := sql.Open("mysql", dbCreds)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return false
+	defer db.Close()
+
+	var r string
+	err = db.QueryRow("SELECT email FROM adults WHERE email = ?", u).Scan(&r)
+
+	if err != nil {
+		return false
+	} else if u == r {
+		return true
+	} else {
+		return false
+	}
 }
 
 func loginGet(w http.ResponseWriter, req *http.Request, e error) {
@@ -90,62 +80,64 @@ func loginPost(w http.ResponseWriter, req *http.Request) {
 	pwd := req.FormValue("pwd")
 	persist := req.FormValue("persist") == "on"
 
-	// confirmation that pwd exists
-	// scan result into p
-	// TODO change this for getUserFromEmail()
-	c, err := getUserFromEmail(uname)
-	if err != nil {
-		// password error - redirect to /login
-		http.Redirect(w, req, "/login", http.StatusTemporaryRedirect)
+	if !isUser(uname) {
+		tpl.ExecuteTemplate(w, "login.html", nil)
+		return
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(pwd), []byte(c.Pwd))
-	if err == bcrypt.ErrMismatchedHashAndPassword {
+	// confirmation that pwd exists
+	// scan result into p
+	u, err := getUserFromEmail(uname)
+	if err != nil {
+		// password error - redirect to /login
+		tpl.ExecuteTemplate(w, "login.html", nil)
+		return
+	}
 
-		// what do I do with loginfail?
-		// redirect to loginfail, collect some hidden form data and then
-		// redirect to /login
-		http.Redirect(w, req, "/loginfail", http.StatusTemporaryRedirect)
-	} else { // succesful password
+	r := CheckPasswordHash(pwd, u.Pwd)
+	if !r {
+		// TODO - change this to loginfail - which is login with an error message
+		tpl.ExecuteTemplate(w, "login.html", nil)
+		return
+	}
 
-		// set a cookie - sid
-		i, err := uuid.NewV4()
-		if err != nil {
-			http.Error(w, "UUID Failed", 500)
-		}
-		id := i.String()
+	// set a cookie - sid
+	i, err := uuid.NewV4()
+	if err != nil {
+		http.Error(w, "UUID Failed", 500)
+	}
+	id := i.String()
 
-		cook := http.Cookie{
-			Name:  "sid",
-			Value: id,
-			Path:  "/",
-		}
+	cook := http.Cookie{
+		Name:  "sid",
+		Value: id,
+		Path:  "/",
+	}
 
-		if persist { // if the user has selected remember me - persistent cookie
-			cook.MaxAge = int(365 * 24 * time.Hour)
-			http.SetCookie(w, &cook)
+	if persist { // if the user has selected remember me - persistent cookie
+		cook.MaxAge = int(365 * 24 * time.Hour)
+		http.SetCookie(w, &cook)
 
-		} else { // forget me - session cookie
-			http.SetCookie(w, &cook)
+	} else { // forget me - session cookie
+		http.SetCookie(w, &cook)
 
-		}
+	}
 
-		db, err := sql.Open("mysql", dbCreds)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
+	db, err := sql.Open("mysql", dbCreds)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-		// add this succesful login to the session database
-		stmt, err := db.Prepare("INSERT INTO sessions (sid, user) VALUES (?, ?)")
-		if err != nil {
-			http.Error(w, "Statement preparation failed", 500)
-		}
-		// Write to database
-		_, err = stmt.Exec(id, uname)
-		if err != nil {
-			http.Error(w, "Statement execution failed", 500)
-		}
+	// add this succesful login to the session database
+	stmt, err := db.Prepare("INSERT INTO sessions (sid, user) VALUES (?, ?)")
+	if err != nil {
+		http.Error(w, "Statement preparation failed", 500)
+	}
+	// Write to database
+	_, err = stmt.Exec(id, uname)
+	if err != nil {
+		http.Error(w, "Statement execution failed", 500)
 	}
 
 	http.Redirect(w, req, "/", http.StatusTemporaryRedirect)
@@ -166,4 +158,16 @@ func logout(w http.ResponseWriter, req *http.Request) {
 
 	http.Redirect(w, req, "/", http.StatusTemporaryRedirect)
 
+}
+
+// HashPassword should have some documentation
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// CheckPasswordHash should have some documentation
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
